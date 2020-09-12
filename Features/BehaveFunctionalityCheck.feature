8ca303949f3fb93ae97a5dec5dbbbbdccf7a2729
akhi1212 <akhilesh08gairola@gmail.com>
2020-09-13 00:29:29 +0530

    This is minor update  in the project file BDD framework.

---
+++ Features/BehaveFunctionalityCheck.feature
@@ -0,0 +1,7 @@
+Feature:  This feature file is used to checkup health of bdd framework
+
+
+  Scenario: General Health checkup of bdd framework and its configuration
+    Given Precondition behave installed and perform its operation
+    When  Run feature file with  the behave
+    Then  it should run successfully from different file setup
--- Features/Steps/Healthcheckupbdd.py
+++ Features/Steps/Healthcheckupbdd.py
@@ -3,16 +3,20 @@ from behave import *
 
 @given(u'Precondition behave installed and perform its operation')
 def step_impl(context):
-   print("I am runned from given")
+   print("I am run from given")
 
 
 @when(u'Run feature file with  the behave')
 def step_impl(context):
-    print("I am runned from given")
+    print("I am run from WHEN STATEMENT")
 
 @then(u'it should run successfully')
 def step_impl(context):
-    print("I am runned from given")
+    print("I am run from THEN STATEMENT")
+
+@then(u'it should run successfully from different file setup')
+def step_impl(context):
+  print("This is testing THEN statement in two different feature file but step class is same. Thanks for executing me")
 
 
 ## from command line, you can use the following:
